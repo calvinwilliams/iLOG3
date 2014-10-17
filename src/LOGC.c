0a6cd2134d8bfb0d0f67cf494916893eebc6fcfc
calvinwilliams <calvinwilliams.c@gmail.com>
2014-10-17 23:55:08 +0800

    UPDATE TO V1.0.11H

--- src/LOGC.c
+++ src/LOGC.c
@@ -123,9 +123,6 @@ static int WriteLogBase( int log_level , char *c_filename , long c_fileline , ch
 		
 		WRITE( fd , log_buffer , log_buflen );
 		
-#if ( defined __linux__ ) || ( defined __unix ) || ( defined _AIX )
-		fsync( fd );
-#endif
 		CLOSE( fd );
 	}
 	
